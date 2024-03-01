61a1b064fc82ff77531d6bc3be186b593312707c
eaejaum <jpedroalmeida179@gmail.com>
2024-03-01 10:10:28 -0300

    Select Menu

---
+++ Select Menu/script.js
@@ -0,0 +1,27 @@
+var selectField = document.getElementById("selectField");
+var selectText = document.getElementById("selectText");
+var options = document.getElementsByClassName("options");
+var list = document.getElementById("list");
+var arrowIcon = document.getElementById("arrowIcon");
+
+for(option of options) {
+    
+    option.onclick = function(){
+        
+        selectText.innerHTML = this.textContent;
+        list.classList.toggle("hide");
+        
+    }
+}
+
+selectField.onclick = function() {
+    
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+
+}
+options.onclick = function() {
+
+
+
+}
